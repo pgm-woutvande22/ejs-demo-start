68cd5eee510554f1dd9ff72b9acc45786bf6e4c0
pgm-woutvande22 <wout.vanderbauwhede@student.arteveldehs.be>
2025-02-18 09:33:00 +0100

    inital commit

--- src/app.js
+++ src/app.js
@@ -5,13 +5,20 @@ import path from "path";
 // create an instance of express
 const app = express();
 
+//set the view engine to ejs
+app.set("view engine", "ejs");
+app.set("views", path.resolve("src", "views"));
+
 // serve static files from the public folder
 // they can be accessed from the root of the site (e.g. /assets/images/dino_07.png) 🦕
 app.use(express.static("public"));
 
 // GET route to serve the index.html file
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("src", "views", "index.html"));
+  res.render("home", {
+    title: "Dino are cool",
+    content: "Dino dino dino dino dino"
+  })
 });
 
 // start the server, listen on port defined in .env file
